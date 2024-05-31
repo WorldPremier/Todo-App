3a62c8c9e872982242098a50b4e280de8d7ead2c
Franklyn Reyes <franklynreyes777@yahoo.com>
2024-05-31 11:36:21 -0400

    Adding todo app in React js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoTable from "./components/TodoTable";
+import React, { useState } from "react";
+import NewTodoForm from "./components/NewTodoForm";
 
 function App() {
+  const [showAddTodoForm, setShowAddTodoForm] = useState(false);
+
+  // destructuring an array
+  const [todos, setTodos] = useState([
+    { rowNumber: 1, rowDescription: "Feed birds", rowAssigned: "User One" },
+    { rowNumber: 2, rowDescription: "Read book", rowAssigned: "User Two" },
+    {
+      rowNumber: 3,
+      rowDescription: "Go for a run",
+      rowAssigned: "User One",
+    },
+    {
+      rowNumber: 4,
+      rowDescription: "Charge  phone",
+      rowAssigned: "Useer Three",
+    },
+  ]);
+
+  // Adding todos as a variable
+  const addTodo = (description, assigned) => {
+    let rowNumber = 0;
+    if (todos.length > 0) {
+      rowNumber = todos[todos.length - 1].rowNumber + 1;
+      // console.log("Our addTodo btn has been clicked!");
+    } else {
+      rowNumber = 1;
+    }
+    const newTodo = {
+      // rowNumber: todos.length + 1,
+      rowNumber: rowNumber,
+      rowDescription: description,
+      rowAssigned: assigned,
+    };
+    // todos.push(newTodo);
+    // console.log(todos);
+    setTodos((todos) => [...todos, newTodo]);
+  };
+
+  const deleteTodo = (deleteTodoRowNumber) => {
+    // filters will be a new set of arrays
+    let filtered = todos.filter(function (value) {
+      return value.rowNumber !== deleteTodoRowNumber;
+    });
+    setTodos(filtered);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">Your Todo's</div>
+        <div className="card-body">
+          <TodoTable todos={todos} deleteTodo={deleteTodo} />
+          {/* // when adding addTodo() with paranthesis as a function, it gets */}
+          {/* auto rendered which is not what we want. Instead we want // it to be
+            a pointer of reference for addTodo instead based on event triggers. */}
+          <button
+            onClick={() => setShowAddTodoForm(!showAddTodoForm)}
+            className="btn btn-primary"
+          >
+            {/* //conditional rendering */}
+            {showAddTodoForm ? "Close New Todo" : "New Todo"}
+          </button>
+          {showAddTodoForm && <NewTodoForm addTodo={addTodo} />}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/NewTodoForm.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+function NewTodoForm(props) {
+  const [description, setDescription] = useState("");
+  const [assigned, setAssigned] = useState("");
+
+  const submitTodo = () => {
+    if (description !== "" && assigned !== "") {
+      props.addTodo(description, assigned);
+      setAssigned("");
+      setDescription("");
+    }
+  };
+  // const descriptionChange = (event) => {
+  //   console.log("description", event.target.value);
+  //   setDescription(event.target.value);
+  // };
+
+  const assignedChange = (event) => {
+    console.log("assigned", event.target.value);
+    setAssigned(event.target.value);
+  };
+
+  return (
+    <div className="mt-5">
+      <form>
+        <div className="mb-3">
+          <label className="form-label">Assigned</label>
+          <div>
+            <input
+              type="text"
+              classsName="form-control"
+              required
+              //setting state in concruent to user's input
+              onChange={assignedChange}
+              // OnChange={(e) => setAssigned(e.target.value)}
+              //printing back to the value
+              value={assigned}
+            ></input>
+          </div>
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Description</label>
+          <textarea
+            className="form-control"
+            rows={3}
+            required
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+          ></textarea>
+        </div>
+        <button
+          type="button"
+          className="btn btn-primary mt-3"
+          onClick={submitTodo}
+        >
+          Add Todo
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default NewTodoForm;
