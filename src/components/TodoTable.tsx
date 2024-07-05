f0a46bf03bda90fb74aa812a41eab7025309ddd9
Franklyn Reyes <franklynreyes777@yahoo.com>
2024-07-04 23:29:48 -0400

    Removing comments

--- src/components/TodoTable.tsx
+++ src/components/TodoTable.tsx
@@ -12,23 +12,7 @@ export const TodoTable: React.FC<{todos: TodoModel[], deleteTodo: Function}> =(p
         </tr>
       </thead>
       <tbody>
-        {/* // Given that we're not passing anything within todo then, props allows */}
-        {/* to be called instead. */}
-        {/* <TodoRowItem
-          rowNumber={props.todos[0].rowNumber}
-          rowDescription={props.todos[0].rowDescription}
-          rowAssigned={props.todos[0].rowAssigned}
-        />
-        <TodoRowItem
-          rowNumber={props.todos[1].rowNumber}
-          rowDescription={props.todos[1].rowDescription}
-          rowAssigned={props.todos[1].rowAssigned}
-        />
-        <TodoRowItem
-          rowNumber={props.todos[2].rowNumber}
-          rowDescription={props.todos[2].rowDescription}
-          rowAssigned={props.todos[2].rowAssigned}
-        /> */}
+       
         {props.todos.map(
           (todo: {
             // key: number;
@@ -50,35 +34,3 @@ export const TodoTable: React.FC<{todos: TodoModel[], deleteTodo: Function}> =(p
   );
 }
 
-// export default TodoTable;
-
-
-// import React from "react"
-// import TodoRowItem  from "./TodoRowItem"
-
-// export const TodoTable: React.FC<{
-//     todos: TodoModel[], deleteTodo: Function
-// }> = (props) => {
-//     return (
-//         <table className="table table-hover">
-//             <thead>
-//                 <tr>
-//                     <th scope='col'>#</th>
-//                     <th scope='col'>Description</th>
-//                     <th scope='col'>Assigned</th>
-//                 </tr>
-//             </thead>
-//             <tbody>
-//                 {props.todos.map(todo => (
-//                     <TodoRowItem
-//                         key={todo.rowNumber} 
-//                         rowNumber={todo.rowNumber}
-//                         rowDescription={todo.rowDescription}
-//                         rowAssigned={todo.rowAssigned}
-//                         deleteTodo={props.deleteTodo}
-//                     />
-//                 ))}
-//             </tbody>
-//         </table>
-//     )
-// }
